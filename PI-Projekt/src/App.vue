91fbd7b2570a0a585232574fb9763d7383bbb21e
BobGraditel <wulfgar680@gmail.com>
2025-06-25 18:40:21 +0200

    Nesto
    Napravio sam poredak i stavio sve basic file-ove, dodao sam template i bla bla bla u kratko nista

---
+++ PI-Projekt/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NavBar /> <!-- ako imaš navigaciju -->
+    <router-view /> <!-- ovdje će se prikazivati komponente ovisno o ruti -->
+    <Footer /> <!-- ako imaš footer -->
+  </div>
+</template>
+
+<script setup>
+import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
+</script>
--- PI-Projekt/src/router/index.js
+++ PI-Projekt/src/router/index.js
@@ -1,11 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Login from '../components/Login.vue';
-import Register from '../components/Register.vue';
+
+
+// importirane komponente za rute
+import Login from '../components/Auth/Login.vue';
+import Register from '../components/Auth/Register.vue';
+import Marketplace from '../components/Marketplace.vue';
+import Profile from '../components/Auth/Profile.vue';
+import BookDetails from '../components/Knjige/BookDetails.vue';
 
 const routes = [
-  { path: '/', redirect: '/login' },
+  { path: '/', redirect: '/marketplace' },
   { path: '/login', component: Login },
-  { path: '/register', component: Register}
+  { path: '/register', component: Register },
+  { path: '/marketplace', component: Marketplace },
+  { path: '/profile', component: Profile },
+  { path: '/book/:id', component: BookDetails, props: true },
 ];
 
 const router = createRouter({
